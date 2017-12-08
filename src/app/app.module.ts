7a0e3340d39b2b0215d6a98c23331c53649363a3
Corrado98 <corradoparisi@hotmail.com>
2017-12-08 16:20:39 +0100

    Add hero service and asynchrounous call to retrieve heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-- NgModel lives here
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import {HeroService} from './hero.service'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component'; // <-
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
