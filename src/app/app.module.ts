f295ad4f430426dc8f592bec4b2f4714edcff5b1
Corrado Parisi <corradoparisi@hotmail.com>
2017-12-10 10:50:35 +0100

    Replace mock heroes with mock server call to in memory data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import {HeroService} from './services/hero-service/hero.service';
+import { HeroService } from './services/hero-service/hero.service';
 import { MessagesComponent } from './messages/messages.component';
-import {MessageService} from './services/message-service/message.service';
-import {AppRoutingModule} from './app-routing.module';
+import { MessageService } from './services/message-service/message.service';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/data-service/InMemoryDataService';
 
 @NgModule({
   declarations: [
@@ -23,9 +26,19 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    ),
+
   ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
