a8b3935fbb8c1479d600950e28bb68ed84e90010
Corrado Parisi <corradoparisi@hotmail.com>
2018-01-01 22:05:48 +0100

    Add deleteHero method

--- src/app/services/hero-service/hero.service.ts
+++ src/app/services/hero-service/hero.service.ts
@@ -12,15 +12,31 @@ export class HeroService {
   // private heroesUrl = 'http://localhost:8080/demo/all';  // URL to web api
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+
   constructor(private messageService: MessageService,
               private http: HttpClient) {
   }
 
   /** POST: add a new hero to the server */
-  addHero (hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero).pipe(
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
       tap((addedHero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
       catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  /** DELETE: delete the hero from the server */
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
     );
   }
 
@@ -46,10 +62,7 @@ export class HeroService {
 
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
-    return this.http.put(this.heroesUrl, hero, httpOptions)
+    return this.http.put(this.heroesUrl, hero, this.httpOptions)
       .pipe(
         tap(_ => this.log(`updated hero id=${hero.id}`)),
         catchError(this.handleError<any>('updateHero'))
